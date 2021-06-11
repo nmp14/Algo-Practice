b895e59f5ed9fddcdf727260ca8baeb42f5b5c7a
Nathan Peek <nmp14fsu@gmail.com>
2021-06-11 17:13:05 -0400

    Add insertion sort

--- sorts.js
+++ sorts.js
@@ -18,3 +18,17 @@ const bubbleSort = (nums) => {
 }
 
 console.log(bubbleSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+
+const insertionSort = arr => {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (arr[i] < arr[j]) {
+                const spliced = arr.splice(i, 1);
+                arr.splice(j, 0, spliced[0]);
+            }
+        }
+    }
+    return arr;
+};
+
+console.log(insertionSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
