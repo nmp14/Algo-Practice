cf3e76e98b3b7d0201321440d49db94208e19e99
Nathan Peek <nmp14fsu@gmail.com>
2021-06-26 02:00:03 -0400

    add example of sliding window

---
+++ sliding-window.js
@@ -0,0 +1,16 @@
+function maxSumArr(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(tempSum, maxSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSumArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 23, 11], 3));
